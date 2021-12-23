43d83ad9d9aa7379dbb22008a60fcb4600ca2640
Patg13 <patg97@hotmail.com>
2021-12-23 22:02:30 +0000

    Database download script now download database from mycobank (for funguild script) and from stbates (for guilds1.1)

--- download_database.sh
+++ download_database.sh
@@ -9,4 +9,13 @@
 
 
 download_date=$( date '+%b-%Y' )
+
+# Database used in FUNGuild.py script
 wget 'https://mycoportal.org/fdex/services/api/db_return.php?dbReturn=Yes&pp=1' -O FUNguild_db_mycoportal.$download_date.json
+
+# Database used in Guilds_v1.1.py
+wget 'http://www.stbates.org/funguild_db_2.php' -O FUNguild_db2_stbates.$download_date.json
+grep "taxon" FUNguild_db_stbates.$download_date.json > FUNguild_db_stbates.$download_date.json.temp
+rm FUNguild_db_stbates.$download_date.json
+mv FUNguild_db_stbates.$download_date.json.temp FUNguild_db_stbates.$download_date.json
+sed -i 's/<\/body>//g' FUNguild_db_stbates.$download_date.json
